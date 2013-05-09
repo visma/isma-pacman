f568f97db64b4ed0708176e6f27fdf19c9b75da4
ismael mameche <ismael.romathier@gmail.com>
2013-05-09 15:37:34 +0200

    cleaning...

--- src/main/java/org/isma/pacman/entity/RankedPlayer.java
+++ src/main/java/org/isma/pacman/entity/RankedPlayer.java
@@ -8,20 +8,4 @@ public class RankedPlayer {
         this.name = name;
         this.score = score;
     }
-
-//    public String toString(int rank, int maxNameLen, int maxScoreLen) {
-//        final String minimalSpace = "  ";
-//        String nameSpace = "";
-//        String scoreSpace = "";
-//
-//        for (int i = 0; i < maxNameLen - name.length(); i++) {
-//            nameSpace += " ";
-//        }
-//        for (int i = 0; i < maxScoreLen - Long.toString(score).length(); i++) {
-//            scoreSpace += " ";
-//        }
-//        return String.format("%s. %s%s%s%s%s pts", rank, name, nameSpace, minimalSpace, scoreSpace, score);
-//    }
-
-
 }
